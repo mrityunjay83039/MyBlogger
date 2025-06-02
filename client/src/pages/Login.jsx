e41e277299ae3ae319c5ca9aa86ff1edc6a6092b
Mrityunjay Kumar Mishra <Mrityunjay.Mishra@q3infotech.net>
2025-06-02 05:51:43 +0530

    completed login and register page ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,12 @@
-
-
 const Login = () => {
   return (
-    <div>
-      Login page
-    </div>
-  )
-}
+    <form className="login" onSubmit={() => {}}>
+      <h1>Login</h1>
+      <input type="text" placeholder="username" value="" />
+      <input type="password" placeholder="password" value="" />
+      <button>Login</button>
+    </form>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
+  const register = () => {};
   return (
-    <div>
-      Register Page
-    </div>
-  )
-}
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input type="text" placeholder="username" value="" />
+      <input type="password" placeholder="password" value="" />
+      <button>Register</button>
+    </form>
+  );
+};
 
-export default Register
+export default Register;
