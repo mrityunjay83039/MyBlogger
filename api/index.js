1175f3f7c95299a567cc39d44d83d9ad91ef6520
Mrityunjay Kumar Mishra <Mrityunjay.Mishra@q3infotech.net>
2025-06-07 12:55:42 +0530

    Worked on login page and login api

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const UserModel = require("./models/UserModel");
 const app = express();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 dotenv.config();
 
 app.use(express.json());
@@ -13,12 +13,13 @@ app.use(cors());
 mongoose.connect(process.env.DB_CONNECTION);
 const salt = bcrypt.genSaltSync(10);
 
+// User registration
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, salt);
-  
+
   try {
-    const user = await UserModel.create({ username, password:hashedPassword });
+    const user = await UserModel.create({ username, password: hashedPassword });
     res.status(201).json({
       success: true,
       message: "User created successfully",
@@ -34,6 +35,34 @@ app.post("/register", async (req, res) => {
   const user = await UserModel.create({ username, password });
   res.json(user);
   res.end();
+});
+
+// User login
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+  const isPasswordSame = bcrypt.compareSync(password, user.password);
+  try {
+    if (isPasswordSame) {
+      res.status(200).json({
+        success: true,
+        message: "User logged in successfully",
+        data: user,
+      });
+    } else {
+      res.status(401).json({
+        success: false,
+        message: "Invalid username or password",
+        data: null,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Invalid request",
+      data: error,
+    });
+  }
 });
 
 app.listen(5000, () => {
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:5000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await res.json();
+      alert(data?.message);
+      console.log("loginres: ", data);
+    } catch (error) {
+      console.log("error while login: ", error);
+    }
+  };
+
   return (
-    <form className="login" onSubmit={() => {}}>
+    <form className="login" onSubmit={handleLogin}>
       <h1>Login</h1>
-      <input type="text" placeholder="username" value="" />
-      <input type="password" placeholder="password" value="" />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Login</button>
     </form>
   );
