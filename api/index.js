645c88bc225dfddbb5f49d2d9c40f3507eb47663
Mrityunjay Kumar Mishra <Mrityunjay.Mishra@q3infotech.net>
2025-06-07 12:02:07 +0530

    created model, schema and setup monogodb connection to create users

--- api/index.js
+++ api/index.js
@@ -1,9 +1,37 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const UserModel = require("./models/UserModel");
 const app = express();
+dotenv.config();
 
+app.use(express.json());
+app.use(cors());
 
+mongoose.connect(process.env.DB_CONNECTION);
 
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await UserModel.create({ username, password });
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Invalid request",
+      data: error,
+    });
+  }
+  const user = await UserModel.create({ username, password });
+  res.json(user);
+  res.end();
+});
 
-app.listen(5000, ()=>{
-    console.log("Server is running on port 5000");
-})
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  const register = () => {};
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:5000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    console.log("res: ", res);
+  };
+
   return (
     <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input type="text" placeholder="username" value="" />
-      <input type="password" placeholder="password" value="" />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Register</button>
     </form>
   );
